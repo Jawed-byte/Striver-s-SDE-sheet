949c3a1ff4377db1625162acc6d61da374f447fc
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-06-05 15:33:46 +0530

    Create L_121_Best Time to buy and sell stock.cpp

---
+++ L_121_Best Time to buy and sell stock.cpp
@@ -0,0 +1,33 @@
+/*----------------- O(n^2) ---------------------
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res=0;
+        int n=prices.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                res=max(res,prices[j]-prices[i]);
+            }
+        }
+        return res;
+    }
+};
+
+-------------------------------------------------*/
+
+// ----------------- O(n) -------------------
+
+class Solution{
+public:
+    int maxProfit(vector<int>& prices){
+        int maxPro=0;
+        int minPrice=prices[0];
+        int n=prices.size();
+        for(int i=0;i<n;i++){
+            minPrice=min(minPrice,prices[i]);
+            maxPro=max(maxPro,prices[i]-minPrice);
+        }
+        return maxPro;
+    }
+};
