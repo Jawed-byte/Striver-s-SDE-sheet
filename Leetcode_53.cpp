72bdb2659ff64112db8b100532c22c5d83331a31
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-06-05 14:40:35 +0530

    Create Leetcode_53.cpp

---
+++ Leetcode_53.cpp
@@ -0,0 +1,58 @@
+/* ---------------- O(n^3) -----------------
+
+class Solution{
+public:
+    int maxSubArray(vector<int>& nums){
+        int n=nums.size();
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    sum+=nums[k];
+                    ans=max(ans,sum);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+----------------------------------------------*/
+
+/* ------------------- O(n^2) ------------------
+
+class Solution{
+public:
+    int maxSubArray(vector<int>& nums){
+        int n=nums.size();
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                ans=max(ans,sum);
+            }
+        }
+        return ans;
+    }
+};
+
+--------------------------------------------------*/
+
+// --------- O(n) -> kadane's Algo --------------
+
+class Solution{
+public:
+    int maxSubArray(vector<int>& nums){
+        int n=nums.size();
+        int ans=INT_MIN;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            ans=max(ans,sum);
+            sum=max(sum,0);
+        }
+        return ans;
+    }
+};
