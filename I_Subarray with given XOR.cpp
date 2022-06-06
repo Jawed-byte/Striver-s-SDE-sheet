796530371fd50cb295264fcb83c796cd68fe6ba0
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-06-06 20:13:50 +0530

    Create I_Subarray with given XOR.cpp

---
+++ I_Subarray with given XOR.cpp
@@ -0,0 +1,14 @@
+int Solution::solve(vector<int> &A, int B) {
+    unordered_map<int,int> freq;
+    int cnt=0;
+    int xorr=0;
+    for(auto it: A){
+        xorr = xorr^it;
+        if(xorr==B)
+          cnt++;
+        if(freq.find(xorr^B)!=freq.end())
+          cnt+=freq[xorr^B];
+        freq[xorr]+=1;
+    }
+    return cnt;
+}
