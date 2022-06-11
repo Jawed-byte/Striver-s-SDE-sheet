8df57520a1e4972daab2c4408de6b0800ae5ad7d
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-06-11 09:47:44 +0530

    Create L_94. Binary Tree Inorder Traversal.cpp

---
+++ L_94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,44 @@
+// Recursive
+
+class Solution {
+public:
+    void solve(TreeNode* node, vector<int>& ans){
+        if(node==NULL)
+            return;
+        solve(node->left, ans);
+        ans.push_back(node->val);
+        solve(node->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
+
+
+// Iterative
+
+class Solution{
+public:
+    vector<int> inorderTraversal(TreeNode* root){
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        while(1){
+            if(node!=NULL){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                if(st.empty())
+                    break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return ans;
+    }
+};
