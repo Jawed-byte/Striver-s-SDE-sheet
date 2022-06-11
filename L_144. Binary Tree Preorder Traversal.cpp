1011ea11768deb4539e182a6f015af0a6dc4756d
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-06-11 11:21:41 +0530

    Create L_144. Binary Tree Preorder Traversal.cpp

---
+++ L_144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,40 @@
+// Recursive
+
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int> &ans){
+        if(root==NULL)
+            return;
+        ans.push_back(root->val);
+        solve(root->left,ans);
+        solve(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root,ans);
+        return ans;
+    }
+};
+
+// Iterative
+
+class Solution{
+public:
+    vector<int> preorderTraversal(TreeNode* root){
+        vector<int> ans;
+        if(root==NULL)
+            return ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right!=NULL)
+                st.push(root->right);
+            if(root->left!=NULL)
+                st.push(root->left);
+        }
+        return ans;
+    }
+};
