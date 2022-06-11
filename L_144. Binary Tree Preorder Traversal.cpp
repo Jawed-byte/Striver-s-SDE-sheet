40262043e77839faaf533c5df0ab3533892a0aed
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-06-11 12:43:05 +0530

    Update L_144. Binary Tree Preorder Traversal.cpp

--- L_144. Binary Tree Preorder Traversal.cpp
+++ L_144. Binary Tree Preorder Traversal.cpp
@@ -2,39 +2,72 @@
 
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int> &ans){
+    void solve(TreeNode* root, vector<int> &preorder){
         if(root==NULL)
             return;
-        ans.push_back(root->val);
-        solve(root->left,ans);
-        solve(root->right,ans);
+        preorder.push_back(root->val);
+        solve(root->left,preorder);
+        solve(root->right,preorder);
     }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        solve(root,ans);
-        return ans;
+        vector<int> preorder;
+        solve(root,preorder);
+        return preorder;
     }
 };
+
 
 // Iterative
 
 class Solution{
 public:
     vector<int> preorderTraversal(TreeNode* root){
-        vector<int> ans;
+        vector<int> preorder;
         if(root==NULL)
-            return ans;
+            return preorder;
         stack<TreeNode*> st;
         st.push(root);
         while(!st.empty()){
             root = st.top();
             st.pop();
-            ans.push_back(root->val);
+            preorder.push_back(root->val);
             if(root->right!=NULL)
                 st.push(root->right);
             if(root->left!=NULL)
                 st.push(root->left);
         }
-        return ans;
+        return preorder;
+    }
+};
+
+// Morris Preorder Traversal
+
+class Solution{
+public:
+    vector<int> preorderTraversal(TreeNode* root){
+        vector<int> preorder;
+        TreeNode* node = root;
+        while(node!=NULL){
+            if(node->left==NULL){
+                preorder.push_back(node->val);
+                node = node->right;
+            }
+            else{
+                TreeNode* l = node->left;
+                while(l->right && l->right!=node){
+                    l = l->right;
+                }
+                if(l->right == NULL){
+                    l->right = node;
+                    preorder.push_back(node->val);
+                    node = node->left;
+                }
+                else{
+                    l->right = NULL;
+                    node = node->right;
+                }
+            }
+        }
+        return preorder;
     }
 };
