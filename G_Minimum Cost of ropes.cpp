94aab6a93e88a2ae6f284a3b7ba612afb9170e00
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-06-13 00:50:39 +0530

    Create G_Minimum Cost of ropes.cpp

---
+++ G_Minimum Cost of ropes.cpp
@@ -0,0 +1,18 @@
+long long minCost(long long arr[], long long n) {
+    priority_queue<long long,vector<long long>,greater<long long>>minh;
+    for(int i=0;i<n;i++)
+    minh.push(arr[i]);
+    
+    long long cost=0;
+    while(minh.size()>=2)
+    {
+        long long first=minh.top();
+        minh.pop();
+        long long second=minh.top();
+        minh.pop();
+        cost+=first+second;
+        minh.push(first+second);
+    }
+    
+    return cost;
+}
