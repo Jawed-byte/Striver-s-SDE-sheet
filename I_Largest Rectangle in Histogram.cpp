18d34b15034a4930dfdb37a12e5805aef0484204
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-06-13 23:26:51 +0530

    Update I_Largest Rectangle in Histogram.cpp

--- I_Largest Rectangle in Histogram.cpp
+++ I_Largest Rectangle in Histogram.cpp
@@ -105,4 +105,28 @@ public:
     }
 };
 
+// Method3
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxA = 0;
+        int n = heights.size();
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || heights[st.top()] >= heights[i])){
+                int h = heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty())
+                    width = i;
+                else
+                    width = i-st.top()-1;
+                maxA = max(maxA,width*h);
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+};
 
