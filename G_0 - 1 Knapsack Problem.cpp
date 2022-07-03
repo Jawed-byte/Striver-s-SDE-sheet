526bd7a36d687ee09114d64d51c5c67974508980
Jawed-byte <51948092+Jawed-byte@users.noreply.github.com>
2022-07-03 16:46:06 +0530

    Update G_0 - 1 Knapsack Problem.cpp

--- G_0 - 1 Knapsack Problem.cpp
+++ G_0 - 1 Knapsack Problem.cpp
@@ -35,14 +35,14 @@
     
     int knapSack(int w, int wt[], int val[], int n){
         int dp[n+1][w+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<w+1;j++){
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=w;j++){
                 if(i==0 || j==0)
                   dp[i][j]=0;
             }
         }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<w+1;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=w;j++){
                 if(wt[i-1]<=j)
                     dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
                 else
